efbbf0411706fc4fa09e8de4b3229bd967e71d29
Stephanie Tam <ru14618@umbc.edu>
2018-11-29 15:54:58 -0500

    updated to allow use of VueMaterial package

--- rescue-system/src/main.js
+++ rescue-system/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 
+Vue.use(VueMaterial)
 // In development mode
 Vue.config.productionTip = false
 
