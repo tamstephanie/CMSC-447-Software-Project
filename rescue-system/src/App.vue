8f0f2289849bb9ded1f6c8759291db4085bbb362
Stephanie Tam <ru14618@umbc.edu>
2018-11-29 15:55:35 -0500

    adding heading to page and formatting for login page

--- rescue-system/src/App.vue
+++ rescue-system/src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
+    <md-card>
+      <md-card-header>
+        <div class="md-title">Rescue Management System</div>
+      </md-card-header>
+    </md-card>
+
+
+
     <img alt="Vue logo" src="./assets/logo.png">
-    <Login title = "Login" />
+    <LoginPage />
   </div>
 </template>
 
 <script>
-import Login from './components/Login'
+import LoginPage from './components/Login'
 
 export default {
   name: 'app',
   components: {
-    Login
+    LoginPage
   }
 }
 </script>
