388812821368b8f65063b4fbdd8736e6d50dd39f
Stephanie Tam <ru14618@umbc.edu>
2018-11-30 04:43:57 -0500

    formatted Nav drawer. need to fix nav linking and body content/routing

--- rescue-system/src/components/pages/MainView.vue
+++ rescue-system/src/components/pages/MainView.vue
@@ -1,63 +1,55 @@
 <template>
   <v-app id="main-view">
-    <v-navigation-drawer :clipped="$vuetify.breakpoint.lgAndUp"
-      v-model="drawer" fixed app>
+    <v-navigation-drawer :clipped="$vuetify.breakpoint.lgAndUp" v-model="drawer" fixed app>
       <v-list dense>
+        <v-list-tile>
+          <v-list-tile-title class="title font-weight-medium">Navigation</v-list-tile-title>    
+        </v-list-tile>
         <template v-for="item in items">
-          <v-layout v-if="item.heading" :key="item.heading"
-            row align-center>
+          <v-layout v-if="item.heading" :key="item.heading" row align-center>
             <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
             </v-flex>
             <v-flex xs6 class="text-xs-center">
               <a href="#!" class="body-2 black--text">EDIT</a>
             </v-flex>
           </v-layout>
-          <v-list-group v-else-if="item.children"
-            v-model="item.model" :key="item.text"
+          <v-list-group
+            v-else-if="item.children"
+            v-model="item.model"
+            :key="item.text"
             :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon="">
+            append-icon
+          >
             <v-list-tile slot="activator">
               <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.text }}
-                </v-list-tile-title>
+                <v-list-tile-title>{{ item.text }}</v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
-            <v-list-tile v-for="(child, i) in item.children"
-              :key="i" on-click="">
+            <v-list-tile v-for="(child, i) in item.children" :key="i" on-click>
               <v-list-tile-action v-if="child.icon">
                 <v-icon>{{ child.icon }}</v-icon>
               </v-list-tile-action>
               <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ child.text }}
-                </v-list-tile-title>
+                <v-list-tile-title>{{ child.text }}</v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
           </v-list-group>
-          <v-list-tile v-else :key="item.text" on-click="">
+          <v-list-tile v-else :key="item.text" on-click>
             <v-list-tile-action>
               <v-icon>{{ item.icon }}</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>
-                {{ item.text }}
-              </v-list-tile-title>
+              <v-list-tile-title>{{ item.text }}</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </template>
       </v-list>
     </v-navigation-drawer>
 
-    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp"
-      color="primary" dark app fixed>
+    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="primary" dark app fixed>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title class="headline font-weight-medium">
-        Rescue Management System
-      </v-toolbar-title>
+      <v-toolbar-title class="headline font-weight-medium">Rescue Management System</v-toolbar-title>
     </v-toolbar>
 
     <v-content>
@@ -71,45 +63,30 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      dialog: false,
-      drawer: null,
-      items: [
-        { icon: 'contacts', text: 'Contacts' },
-        { icon: 'history', text: 'Frequently contacted' },
-        { icon: 'content_copy', text: 'Duplicates' },
-        {
-          icon: 'keyboard_arrow_up',
-          'icon-alt': 'keyboard_arrow_down',
-          text: 'Labels',
-          model: true,
-          children: [
-            { icon: 'add', text: 'Create label' }
-          ]
-        },
-        {
-          icon: 'keyboard_arrow_up',
-          'icon-alt': 'keyboard_arrow_down',
-          text: 'More',
-          model: false,
-          children: [
-            { text: 'Import' },
-            { text: 'Export' },
-            { text: 'Print' },
-            { text: 'Undo changes' },
-            { text: 'Other contacts' }
-          ]
-        },
-        { icon: 'settings', text: 'Settings' },
-        { icon: 'chat_bubble', text: 'Send feedback' },
-        { icon: 'help', text: 'Help' },
-        { icon: 'phonelink', text: 'App downloads' },
-        { icon: 'keyboard', text: 'Go to the old version' }
-      ]
-    }),
-    props: {
-      source: String
-    }
+export default {
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      /*{
+        icon: "keyboard_arrow_up",
+        "icon-alt": "keyboard_arrow_down",
+        text: "Table Views",
+        model: true,
+        children: [
+          { icon: "event_note", text: "Events Table" },
+          { icon: "notification_important", text: "Missions Table" }
+        ]
+      },*/
+      { icon: "event_note", text: "Events Table" },
+      { icon: "notification_important", text: "Missions Table" },
+      { icon: "map", text: "Map View" },
+      { icon: "search", text: "Search" },
+      { icon: "power_settings_new", text: "Log Off" }
+    ]
+  }),
+  props: {
+    source: String
   }
+};
 </script>
