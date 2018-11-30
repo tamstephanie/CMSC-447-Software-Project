c024f9cad35013d46d7292c8ed2dce30fec5aa3c
Stephanie Tam <ru14618@umbc.edu>
2018-11-30 15:16:12 -0500

    added comments to App.vue

--- rescue-system/src/App.vue
+++ rescue-system/src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <v-app id="app">
     <v-content>
-      <LoginPage />
+      <!-- Need to adjust view based on status:
+       if loggedIn => <MainView /> 
+       else <LoginPage /> -->
+      <MainView />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import LoginPage from './components/pages/LoginPage'
+// import LoginPage from './components/pages/LoginPage'
+import MainView from './components/pages/MainView'
 
 export default {
   name: 'App',
   components: {
-    LoginPage
+    // LoginPage
+    MainView
   },
   data() {
     return {
