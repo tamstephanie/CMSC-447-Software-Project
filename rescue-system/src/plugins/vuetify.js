5590c4ede4e0bf40c2c596860d61f7c591722a48
Stephanie Tam <ru14618@umbc.edu>
2018-11-30 01:58:29 -0500

    removed VueMaterial and switched to Vuetify

---
+++ rescue-system/src/plugins/vuetify.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuetify, { colors } from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+  theme: {
+    primary: colors.red.darken4,
+    secondary: '#116e6e'
+  }
+})
