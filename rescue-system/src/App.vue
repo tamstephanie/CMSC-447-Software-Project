248a0c6e761bf4eedeaf0c07bd7319a5d8cdd768
Steph Tam <tam.stephaniek@gmail.com>
2018-12-01 21:09:40 -0500

    attempted to adjust v-content to fix padding

--- rescue-system/src/App.vue
+++ rescue-system/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="app">
-    <v-content>
+    <v-content flex> <!-- style="padding: 64px 0px 0px 250px" -->
       <!-- Need to adjust view based on status:
        if loggedIn => <MainView /> 
        else <LoginPage /> -->
