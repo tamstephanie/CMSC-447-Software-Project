5e97ffdc660aa88a7138baf2b0ec57200bae2069
Steph Tam <tam.stephaniek@gmail.com>
2018-12-01 23:40:05 -0500

    begin Event creation page

---
+++ rescue-system/src/components/views/CreateEvent.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card>
+    <form class="pa-4">
+      <v-text-field
+        v-model="address"
+        :error-messages="addressError"
+        label="Address"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+      <v-text-field
+        v-model="victims"
+        :error-messages="victimsError"
+        label="# of Victims"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+    </form>
+  </v-card>
+</template>
