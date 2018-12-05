e370d0595ff0fbead8f6dd39e923975827ea4ea6
Stephanie Tam <ru14618@umbc.edu>
2018-12-05 16:12:36 -0500

    renamed paths and working on links in nav bar

--- rescue-system/src/helpers/router.js
+++ rescue-system/src/helpers/router.js
@@ -3,8 +3,8 @@ import Router from 'vue-router';
 
 // Import local files
 import LoginPage from '../login/LoginPage'
-import { CreateEvent, EventsTable, Event } from '../main/events/.'
-import { CreateMission, MissionsTable } from '../main/missions/.'
+import { CreateEvent, EventsTable } from '../main/events/.'
+import MissionsTable from '../main/missions/MissionsTable'
 
 Vue.use(Router);
 
@@ -13,34 +13,32 @@ export const router = new Router({
   mode: 'history',
   routes: [
     { path: '/', component: LoginPage },
-    { path: '/createEvent', component: CreateEvent },
-    { path: '/eventsTable', component: EventsTable },
-    { path: '/eventsTable/event', component: Event },
-    { path: '/createMission', component: CreateMission },
-    { path: '/missionsTable', component: MissionsTable },
-    /* Need more paths to handle MainView and
-    subviews (EventsTable, MissionsTable, EventCreation, MissionCreation, MapView) */
-
+    { path: '/create-event', component: CreateEvent },
+    { path: '/events', component: EventsTable },
+    // { path: '/eventsTable/event', component: Event },
+    { path: '/missions', component: MissionsTable },
+    { path: '/map', component: MapView },
+    
     // If path doesn't exist, redirect to main page after logging in?
     { path: '*', redirect: '/' }
   ]
 });
 
-<li class="nav-item">
-  <router-link class="nav-link" to="/createEvent">Create Event</router-link>
-</li>
-<li class="nav-item">
-  <router-link class="nav-link" to="/eventsTable">Events</router-link>
- </li>
-<li class="nav-item">
-   <router-link class="nav-link" to="/eventsTable/event">Event</router-link>
- </li>
-<li class="nav-item">
-  <router-link class="nav-link" to="/createMission">Create Mission</router-link>
-</li>
-<li class="nav-item">
-  <router-link class="nav-link" to="/missionsTable">Missions</router-link>
- </li>
+// <li class="nav-item">
+//   <router-link class="nav-link" to="/createEvent">Create Event</router-link>
+// </li>
+// <li class="nav-item">
+//   <router-link class="nav-link" to="/eventsTable">Events</router-link>
+//  </li>
+// <li class="nav-item">
+//    <router-link class="nav-link" to="/eventsTable/event">Event</router-link>
+//  </li>
+// <li class="nav-item">
+//   <router-link class="nav-link" to="/missionsTable">Missions</router-link>
+//  </li>
+//  <li class="nav-item">
+//   <router-link class="nav-link" to="/map">MapView</router-link>
+// </li>
 
 router.beforeEach((to, from, next) => {
   // Redirect to login page if not logged in and trying to access a restricted page
