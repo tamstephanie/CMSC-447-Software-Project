9e9bea4a374fc9d2008a5cd50cb5f2d3bf050f2c
Stephanie Tam <ru14618@umbc.edu>
2018-12-05 16:11:57 -0500

    added Cancel and Submit buttons. need to work on redirects

--- rescue-system/src/main/events/CreateEvent.vue
+++ rescue-system/src/main/events/CreateEvent.vue
@@ -67,7 +67,11 @@
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="#A9A9A9">Cancel</v-btn>
-        <v-btn color="primary" class="ml-3">Submit</v-btn>
+        <v-btn
+          color="primary"
+          class="ml-3"
+          to="/events"
+        >Submit</v-btn>
       </v-card-actions>
     </form>
   </v-card>
@@ -94,7 +98,9 @@ export default {
       v => !!v || 'A phone number is required',
       v => /\d{3}-\d{3}-\d{4}/.test(v) || 'Please follow the following format: XXX-XXX-XXXX'
     ],
-    notes: ''
+    notes: '',
+    loader: '',
+    loading: false
   })
 };
 </script>
