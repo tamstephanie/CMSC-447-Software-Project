40c56a585aaccc0026519974fceb36494a45d64e
Steph Tam <tam.stephaniek@gmail.com>
2018-12-12 00:25:37 -0500

    trying to customize the path names, but so far, no luck

--- rescue-system/src/helpers/router.js
+++ rescue-system/src/helpers/router.js
@@ -7,6 +7,7 @@ import MainView from '../main/MainView'
 import EventsTable from '../main/events/EventsTable'
 import MissionsTable from '../main/missions/MissionsTable'
 import CreateEvent from '../main/events/CreateEvent'
+import MapView from '../main/map/Map'
 
 Vue.use(Router);
 
@@ -16,12 +17,15 @@ export const router = new Router({
   mode: 'history',
   routes: [
     { path: '/login', name: 'Login', component: LoginPage },
-    { path: '/main', name: 'Main', component: MainView },
-    { path: '/main/events', name: 'Events', component: EventsTable },
-    { path: '/main/missions', name: 'Missions', component: MissionsTable },
+    { path: '/main', name: 'Main', component: MainView,
+      children: [
+        { path: '/events', name: 'Events', component: EventsTable },
+        { path: '/missions', name: 'Missions', component: MissionsTable },
+        { path: '/map', name: 'Map', component: MapView }
+      ]
+    },
     { path: '/create', name: 'Create Events', component: CreateEvent, adminOnly: true },
     // { path: '/eventsTable/event', component: Event },
-    // { path: '/map', component: MapView },
     
     // If path doesn't exist, redirect to main page after logging in?
     { path: '*', redirect: '/login' }
@@ -30,33 +34,8 @@ export const router = new Router({
 
 router.beforeEach((to, from, next) => {
   // Redirect to login page if not logged in and trying to access a restricted page
-  const publicPage = ['/login', '/main', '/create'];
-  const authRequired = !publicPage.includes(to.path);
-  const loggedIn = localStorage.getItem('user');
-
-  if (authRequired && !loggedIn) {
-    return next('/login');
-  }
-
-  next();
-})
-
-
-
-export const router_main = new Router({
-  mode: 'history',
-  routes: [
-    { path: '/main/events', name: 'Events', component: EventsTable },
-    { path: '/main/missions', name: 'Missions', component: MissionsTable },
-    { path: '/create', name: 'Create Events', component: CreateEvent, adminOnly: true },
-    // { path: '/map', component: MapView },
-    { path: '*', redirect: '/login' }
-  ]
-});
-
-router_main.beforeEach((to, from, next) => {
-  // Redirect to login page if not logged in and trying to access a restricted page
-  const publicPage = ['/main/*'];
+  const publicPage = ['/login', '/main', '/events',
+                      '/missions', '/map', '/create'];
   const authRequired = !publicPage.includes(to.path);
   const loggedIn = localStorage.getItem('user');
 
